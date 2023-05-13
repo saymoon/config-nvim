329f9e1362fcd6a959b8eb461f597d65ba17b890
saymoon <saymoon@gmail.com>
2023-05-14 00:10:14 +0800

    init project

---
+++ init.lua
@@ -0,0 +1,29 @@
+local set = vim.o
+vim.opt.swapfile = false
+set.number = true
+set.relativenumber = true
+set.clipboard = "unnamed"
+
+-- highlight the block just copied
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	pattern = { "*" },
+	callback = function()
+		vim.highlight.on_yank({
+			timeout = 300,
+		})
+	end,
+})
+
+-- keybindings
+local opt = { noremap = true, silent = true }
+vim.g.mapleader = " "
+vim.keymap.set("n", "<C-h>", "<C-w>h", opt)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opt)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opt)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opt)
+vim.keymap.set("n", "<Leader>v", "<C-w>v", opt)
+vim.keymap.set("n", "<Leader>s", "<C-w>s", opt)
+
+-- https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
+vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
+vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
