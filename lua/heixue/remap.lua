4b2098c397d00dcc52567be5e258e0c5076d2110
saymoon <saymoon@gmail.com>
2023-05-21 13:38:45 +0800

    add shortcut for lsp code format

--- lua/heixue/remap.lua
+++ lua/heixue/remap.lua
@@ -2,6 +2,7 @@ local opt = { noremap = true, silent = true }
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.keymap.set("n", "<Leader>pv", vim.cmd.Ex, opt)
+
 vim.keymap.set("n", "<C-h>", "<C-w>h", opt)
 vim.keymap.set("n", "<C-j>", "<C-w>j", opt)
 vim.keymap.set("n", "<C-k>", "<C-w>k", opt)
@@ -9,3 +10,4 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", opt)
 vim.keymap.set("n", "<Leader>v", "<C-w>v", opt)
 vim.keymap.set("n", "<Leader>s", "<C-w>s", opt)
 
+vim.keymap.set('n', '<space>f', '<cmd>lua vim.lsp.buf.format()<CR>', opt)
