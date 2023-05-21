717c308131309b9ee29f9e800d670a11e4528441
saymoon <saymoon@gmail.com>
2023-05-21 13:37:39 +0800

    config nvim-cmp with luasnip, buffer, path support

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,10 +1,40 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({ buffer = bufnr })
 end)
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+require('luasnip.loaders.from_vscode').lazy_load()
+
+cmp.setup({
+    sources = {
+        { name = 'path' },
+        { name = 'nvim_lsp' },
+        { name = 'buffer',  keyword_length = 3 },
+        { name = 'luasnip', keyword_length = 2 },
+    },
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+        ['<Tab>'] = cmp_action.luasnip_supertab(),
+        ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+    },
+    preselect = 'item',
+    completion = {
+        completeopt = 'menu,menuone,noinsert'
+    },
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
+})
--- init.lua
+++ init.lua
@@ -57,7 +57,9 @@ require("lazy").setup({
 	    -- Autocompletion
 	    {'hrsh7th/nvim-cmp'},     -- Required
 	    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	    {'L3MON4D3/LuaSnip'},     -- Required
+        {'hrsh7th/cmp-buffer'},
+        {'hrsh7th/cmp-path'},
+	    {'L3MON4D3/LuaSnip', dependencies = { 'rafamadriz/friendly-snippets', 'saadparwaiz1/cmp_luasnip' }},     -- Required
 	  }
 	}
 })
